37e51ffb46de823800d3440cc592f3ffdef4c0f4
Lucas Rizzini <lucasrizzini@hotmail.com>
2023-01-09 18:42:56 -0300

    atualizando

---
+++ Documentos/scripts/download_videos_terminal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+url=$(/usr/bin/xclip -o -selection clipboard)
+echo "$(tput setaf 82)URL: "$url"$(tput sgr0)"
+regex='^https?://[^/]+'
+if [[ $url =~ $regex ]];then
+    if ! /usr/bin/yt-dlp "$url"; then
+        if [[ "$url" == *"reddit.com/r/"* ]];then
+            /usr/bin/xdg-open "https://redditsave.com/info?url=$url" &> /dev/null & disown
+        fi
+    fi
+else
+    /bin/echo "$(tput setaf 197)URL inválido..$(tput sgr0)"
+fi
